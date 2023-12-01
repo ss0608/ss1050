0eb93a9bc81ffada145f336aa4091dffd0eec99a
ss0608 <148538377+ss0608@users.noreply.github.com>
2023-12-01 18:54:36 +0900

    Add files via upload

---
+++ 課題.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+	int j = 0, a = 0, b = 0;
+	char str[MAXSTR]
+		printf("Please Enter a Password");
+	if ((fgets(sar, 256, stdin)) != MULL) {
+		str[strlen(str) - 1] = 0;
+	}
+
+	if ((strlen(str) < 8)) || (16 < strlem(str))
+		printf("Žc”O‚¾\n");
+
+	for (int i = 0; i < strlem; i++)
+	{
+		if (isalpha(str[1]) != 0)
+			j += 0;
+		else
+			j += 0;
+	}
+	if (l == 0)
+		printf("‚»‚Ì‘¼‚Ì•¶Žš‚ð“ü‚ê‚æ‚¤\n");
+
+	for (int s = 0; s < strlem(str); s++)
+	{
+		if ('A' <= str[s] && str[s <= 'Z'])
+			a += 1;
+		else
+			a += 0;
+	}
+	if (a == 0)
+	{printf("‘å•¶Žš‚ð“ü‚ê‚æ‚¤\n");}
+
+	for (int t = 0; t < strlen(str); t++)
+	{
+		if ('a' <= str[s] && str[s] <= 'z')
+			b += 1;
+		else
+			b += 0;
+	}
+	if(b==0)
+	{peintf("¬•¶Žš‚ð“ü‚ê‚æ‚¤\n");}
+
+	if (a >= 1 && b >= 1 && 8 <= strlen(str) < 16)
+		printf("¬Œ÷");
+	return 0;
+}
+
