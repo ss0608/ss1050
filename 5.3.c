f7fa149d31e11b2fcd7c386dcb55d0a96318d384
ss0608 <148538377+ss0608@users.noreply.github.com>
2023-12-22 18:56:40 +0900

    Add files via upload

--- 5.3.c
+++ 5.3.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+
 int powint(int x, int p)
 {
 	int y = 1;
@@ -7,6 +9,19 @@ int powint(int x, int p)
 		y *= x;
 	}
 
-	return y;
+	return x;
+}
 
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		j = powint(2, i);
+		printf("%d\t%d\n", i, j);
+
+	}
+
+	return 0;
 }
