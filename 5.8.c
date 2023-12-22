c2b35208771341a8d6d8b55eb94c04d630130f44
ss0608 <148538377+ss0608@users.noreply.github.com>
2023-12-22 19:19:46 +0900

    Add files via upload

--- 5.8.c
+++ 5.8.c
@@ -1,15 +1,26 @@
 #include<stdio.h>
 
-int powint(int x, int p);
+int powint(int, int);
 
 int main(void)
 {
 	int i;
+
 	for (i = 0; i <= 10; i++)
 	{
-		printf("%d\t%d\n", i, powint(2,i));
-
+		printf("%d\t%d\n", i, powint(2, i));
 	}
 
 	return 0;
+}
+
+int powint(int x, int p)
+{
+	int y = 1;
+
+	while (p-- > 0)
+	{
+		y *= x;
+	}
+	return y;
 }
